0eaeafa1ca6fe4d6389af0bd8dd491c072be59f1
jrmph13 <jhames.martin.dev@gmail.com>
2025-02-26 10:09:22 +0000

    perf(utils): update payload parser

--- furry/src/controllers/schema.ts
+++ furry/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_462428ce369a4014b2757326bfec1afc = () => {
-  return '2025-02-03T10:52:35Z';
+const node_ed48478811b2411e810d8d92dc288216 = () => {
+  return '2025-02-26T10:09:22Z';
 };
