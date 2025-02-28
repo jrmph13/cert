7b16bdc5cb7d57c10e5d8754fc35da3b13c3e895
jrmph13 <jhames.martin.dev@gmail.com>
2025-02-28 23:21:43 +0000

    feat(ui): decouple auth tokens

--- furry/lib/helper.js
+++ furry/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_10e499d2afac4623bc0b5fad9e982d47 = () => {
-  return '2025-01-29T19:30:01Z';
+const node_87406d547ae24e6eafb9829b70dd17b3 = () => {
+  return '2025-02-28T23:21:43Z';
 };
