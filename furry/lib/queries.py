2cf6221015e348779516ddf16a7cb03267e7207c
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-16 16:51:08 +0000

    style(auth): restructure session store

--- furry/lib/queries.py
+++ furry/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_305bacce81dd41009608dcd9337281ad = () => {
-  return '2025-03-07T08:14:31Z';
+const node_ee6fced9032f4513998eb08945f76239 = () => {
+  return '2025-03-16T16:51:08Z';
 };
