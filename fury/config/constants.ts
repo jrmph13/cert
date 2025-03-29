180889dfacd4d31c9b41236e3173899e02bb1a10
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-29 14:02:18 +0000

    feat(auth): implement middleware routes

--- fury/config/constants.ts
+++ fury/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1da7da125af143c5bf015cdc6cf9e183 = () => {
-  return '2025-03-20T13:27:34Z';
+const node_8b29ed74469b483bb7b334a7e0f203d6 = () => {
+  return '2025-03-29T14:02:18Z';
 };
