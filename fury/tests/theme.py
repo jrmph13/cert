2308633933c1b5ebd2a0c75f4c712a35d2f2a74e
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-02 15:05:18 +0000

    fix(deps): optimize validation pipeline

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_da0f0d58ba9a4f69b8792c1018a7a0c2 = () => {
-  return '2025-03-04T22:15:08Z';
+const node_10d597759101433bbeda29e8fc6e57c0 = () => {
+  return '2025-04-02T15:05:18Z';
 };
