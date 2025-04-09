30523b6e5b0c384fa7f9a5e8987c706322d3760d
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-09 10:17:17 +0000

    style(core): decouple validation pipeline

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9920bbb5801d4da8ac9a885f1f4cda5f = () => {
-  return '2025-04-08T05:01:31Z';
+const node_01cc12bf0c6e4e00b1f7341d869de5e1 = () => {
+  return '2025-04-09T10:17:17Z';
 };
