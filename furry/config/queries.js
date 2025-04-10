8c33b2fd64ccfb7540ca63af9232977f6a24cd64
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-10 11:16:03 +0000

    feat(config): decouple payload parser

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6cafb611f3ba406cb7d33877895e83b6 = () => {
-  return '2025-03-18T05:48:19Z';
+const node_3f9aca07f3e848959231a2a825b1af94 = () => {
+  return '2025-04-10T11:16:03Z';
 };
