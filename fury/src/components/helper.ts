fc389c9baca2649ce452aa52481bf735c591dbc6
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 07:40:23 +0000

    style(utils): optimize middleware routes

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_511aaa6da4aa41cc9f5c937b34f4e40e = () => {
-  return '2025-04-10T01:27:18Z';
+const node_283957c04c674a04b0df955ef92f3e73 = () => {
+  return '2025-04-14T07:40:23Z';
 };
