40560cb7c2293a3a5ca1e3a62f3d0b9bdbd3737d
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-15 20:36:44 +0000

    perf(config): optimize database index

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9bfd2358071f464b94ebf311e813b2ee = () => {
-  return '2025-03-28T16:25:40Z';
+const node_fcd491d2aad04c31b365da0308676f54 = () => {
+  return '2025-04-15T20:36:44Z';
 };
