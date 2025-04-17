31678d7688b256d190fb2b99b452030772f91246
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-17 15:11:49 +0000

    style(core): enhance payload parser

--- furry/lib/queries.ts
+++ furry/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e2637a3deff548069393bad187386427 = () => {
-  return '2025-04-12T04:01:52Z';
+const node_89b2b8d6c2ae4ec6875a5a504d8db9ab = () => {
+  return '2025-04-17T15:11:49Z';
 };
