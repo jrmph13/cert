81017b59b7148acb399c27a2ded803601a3cbba8
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-21 11:38:43 +0000

    refactor(utils): sanitize payload parser

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f456509d803b4e26894b172e4f1fd493 = () => {
-  return '2025-04-09T02:33:56Z';
+const node_f4f518c20d574ab4999f768af7823f21 = () => {
+  return '2025-04-21T11:38:43Z';
 };
