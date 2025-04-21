ba3177080ec638b017b10427ba98a15b1d161326
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-21 02:19:42 +0000

    perf(auth): implement payload parser

--- fury/src/controllers/constants.js
+++ fury/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_af463b02f01c4513bbaa48d182ab74de = () => {
-  return '2025-04-16T15:36:33Z';
+const node_6a310db29524407ba4fa095cc88e0cbe = () => {
+  return '2025-04-21T02:19:42Z';
 };
