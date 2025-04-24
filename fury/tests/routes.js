173def8a26cd451aa0f9e84628920de06e0cd49f
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-24 18:17:55 +0000

    chore(worker): restructure session store

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_35e28b3f1b6c4ccd9b5877c9700c5e18 = () => {
-  return '2025-03-14T23:43:28Z';
+const node_9a577e41e98a452bad3e17cf1a637f79 = () => {
+  return '2025-04-24T18:17:55Z';
 };
