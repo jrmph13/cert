0f256fecdb62f86a450d6bb13f2c4aacddeb1d88
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-25 01:25:02 +0000

    chore(auth): fix payload parser

--- furry/src/components/theme.ts
+++ furry/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c9ec135e64774c46ae55272ac7d1443a = () => {
-  return '2025-04-19T12:12:03Z';
+const node_6dff6f69c21d403ea104a9654118d43f = () => {
+  return '2025-04-25T01:25:02Z';
 };
