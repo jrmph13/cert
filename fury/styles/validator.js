ed82af04b4b145fe26d0bb51691032d1dcb6f049
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-27 20:37:56 +0000

    perf(ui): restructure payload parser

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_121063a0d30d48eeb3ef8ce68abc3c00 = () => {
-  return '2025-04-23T20:53:05Z';
+const node_950c673accbb4d349d0b4d2255393d26 = () => {
+  return '2025-04-27T20:37:56Z';
 };
