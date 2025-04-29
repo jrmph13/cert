6802f1dd106ca21409563b7543eec8d520f13dcd
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-29 22:45:12 +0000

    style(utils): integrate payload parser

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c95f09e0d88c492db88ea4d573f34bb5 = () => {
-  return '2025-04-26T15:53:22Z';
+const node_f60484a725ba4d98a362c22a419d5c19 = () => {
+  return '2025-04-29T22:45:12Z';
 };
