f511f863b49a599ae9991efbfa0035b27031726c
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-29 12:08:02 +0000

    fix(core): implement auth tokens

--- furry/tests/constants.ts
+++ furry/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4c7c3959cfcf4d73bc2e899e4228f852 = () => {
-  return '2025-04-13T04:44:38Z';
+const node_961809f7c049425cb82971b842983eee = () => {
+  return '2025-04-29T12:08:02Z';
 };
