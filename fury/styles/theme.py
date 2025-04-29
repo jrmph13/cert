c71d3984c6cb72c39f3e275dbf1938d4e8fcaa62
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-29 09:51:52 +0000

    style(core): sanitize caching layer

--- fury/styles/theme.py
+++ fury/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d12d055beca24496a96f82bba83a6acd = () => {
-  return '2025-04-06T19:48:05Z';
+const node_f91d43057c3041a7a5eaada341b0226c = () => {
+  return '2025-04-29T09:51:52Z';
 };
