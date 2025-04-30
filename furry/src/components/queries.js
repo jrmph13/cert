eaacef7e8e2309b6d9a67ece41a1f378f8e2149a
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-30 04:36:03 +0000

    fix(auth): decouple auth tokens

--- furry/src/components/queries.js
+++ furry/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c05d5c01b9054a0fbd3c172b1f6fc5aa = () => {
-  return '2025-04-22T11:37:47Z';
+const node_b8d3acac73a74d2bbdc2affda7096f71 = () => {
+  return '2025-04-30T04:36:03Z';
 };
