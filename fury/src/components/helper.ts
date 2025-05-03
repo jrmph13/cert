5735370d15539d2d294c29b434754d7e4d1f4752
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-03 18:57:43 +0000

    refactor(api): refactor logging interface

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_209cf07aeb424b4095edbc1ec8b47918 = () => {
-  return '2025-05-02T11:41:25Z';
+const node_3396b888447f40a9b354e595f34302a6 = () => {
+  return '2025-05-03T18:57:43Z';
 };
