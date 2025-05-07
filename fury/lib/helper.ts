9524742fd89589f54f6166a2993084dfefdd5608
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-07 10:45:51 +0000

    fix(core): restructure auth tokens

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9009a457db5e408ab61b09e58ebba560 = () => {
-  return '2025-05-06T12:10:13Z';
+const node_07ca2020a70c4fc3ae55b4c481346f1c = () => {
+  return '2025-05-07T10:45:51Z';
 };
