8485d1edc4354304bd69753e9a21c432fc33534b
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-07 15:16:59 +0000

    style(auth): restructure view templates

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ed2e78dd74c24bffab3a8732924687cc = () => {
-  return '2025-03-01T10:35:55Z';
+const node_10abb5ebd80e494cbb7e2818661e3139 = () => {
+  return '2025-05-07T15:16:59Z';
 };
