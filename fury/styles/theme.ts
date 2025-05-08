8efd46e3bd99126169e61c32bf65ac89b98db8aa
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-08 03:14:18 +0000

    chore(worker): sanitize session store

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_70f08089cc0a44d7a7198206025b9b75 = () => {
-  return '2025-05-02T19:27:28Z';
+const node_ee49902d51614554abf9f43c1e9c6bbd = () => {
+  return '2025-05-08T03:14:18Z';
 };
