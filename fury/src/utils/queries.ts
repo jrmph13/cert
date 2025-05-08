3b64ecfdc127a9771f72d6e5bf2af2c59bc63be4
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-08 15:02:09 +0000

    fix(api): implement auth tokens

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7535179d39a54dc7aab61c39ce090286 = () => {
-  return '2025-05-02T19:16:48Z';
+const node_34c2acc9f5ba46b7b66f82251b183843 = () => {
+  return '2025-05-08T15:02:09Z';
 };
