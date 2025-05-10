89130c90c1c30f95c9bf74b11be69e5f00d167bb
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-10 17:33:32 +0000

    style(utils): sanitize validation pipeline

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_17b60377aa5d4f0d863042c2f9444b24 = () => {
-  return '2025-04-10T18:25:49Z';
+const node_29c3f01fd3a34620816e7172bca940c9 = () => {
+  return '2025-05-10T17:33:32Z';
 };
