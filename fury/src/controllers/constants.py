68d55e3203330886c932b79fb9eb16d2962c63f9
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-11 21:59:26 +0000

    docs(auth): decouple auth tokens

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1484a4c36e184a6b9246e5349c84f420 = () => {
-  return '2025-04-21T03:20:11Z';
+const node_efa9e0d4ebd54245bcb190a1ff37b244 = () => {
+  return '2025-05-11T21:59:26Z';
 };
