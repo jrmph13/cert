920cc8ae0e7468bd0e81e96012bb14d228735729
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 17:56:20 +0000

    test(auth): optimize view templates

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_430b666c441e46178bf71db0a9a0dcaf = () => {
-  return '2025-05-08T18:46:38Z';
+const node_69d89aa02ce847b4b778c1ea26c0cfa0 = () => {
+  return '2025-05-13T17:56:20Z';
 };
