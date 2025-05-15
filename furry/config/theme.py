624d23750606cc50ae2bec89ff4ce88a3ab034c4
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-15 21:28:12 +0000

    feat(core): integrate payload parser

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8111ff5c60ab42f5884ecb0c529f6293 = () => {
-  return '2025-05-04T17:17:16Z';
+const node_8aa5f26f7599470d90050a5c07cb6b16 = () => {
+  return '2025-05-15T21:28:12Z';
 };
