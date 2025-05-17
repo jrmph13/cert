41358060a79af0eabbd8420f24e79dd2c82307cc
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 19:13:54 +0000

    refactor(worker): update middleware routes

--- furry/styles/constants.js
+++ furry/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3caf6edac6ae4bb8ad5d94aaef2df63e = () => {
-  return '2025-04-15T07:16:28Z';
+const node_46e312bce33a43a39ae02ccb5085a8ab = () => {
+  return '2025-05-17T19:13:54Z';
 };
