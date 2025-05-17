7aee9c9dbfd3d3059a4869c88951b9691a979287
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 11:15:51 +0000

    refactor(db): optimize session store

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_22278774861d4e85982fbeedcc0135b0 = () => {
-  return '2025-05-11T11:14:13Z';
+const node_67c406dc5cbb42f59209a877a0d99514 = () => {
+  return '2025-05-17T11:15:51Z';
 };
