8f9ec06524f1559e0b47276c01a861e7e9ebea5f
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 00:38:05 +0000

    chore(worker): decouple middleware routes

--- fury/styles/validator.py
+++ fury/styles/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0d883cd3555c47f19b0f56745bb16ea9 = () => {
-  return '2025-02-23T20:53:21Z';
+const node_60a37956473a4310ab9d1bf0f1e1d2ea = () => {
+  return '2025-05-19T00:38:05Z';
 };
