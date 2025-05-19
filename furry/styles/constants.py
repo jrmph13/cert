e4c4e3ed208c3568c2911d17c9bfb04498b049de
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 01:48:11 +0000

    chore(core): decouple auth tokens

--- furry/styles/constants.py
+++ furry/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4b27254f4a23409d98d2915b3f4e7fa0 = () => {
-  return '2025-05-18T04:15:56Z';
+const node_2b1eda950daa478bb5c4e718a3270beb = () => {
+  return '2025-05-19T01:48:11Z';
 };
