2c7b4baf802663f09987fdfd25d101e4412fe729
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 00:23:54 +0000

    perf(config): sanitize view templates

--- furry/src/components/schema.py
+++ furry/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_da6b464148b44ca59e51326aae807a76 = () => {
-  return '2025-05-06T12:35:00Z';
+const node_1a71d27cd09f4cd89bcd20f52f58b2bd = () => {
+  return '2025-05-19T00:23:54Z';
 };
