6e820f0951774a258c422ed0c51163d3b425abdf
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-24 11:31:03 +0000

    refactor(core): optimize middleware routes

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1df8cde0a6dc4e39945dc693f972d94c = () => {
-  return '2025-05-15T02:32:41Z';
+const node_7a73e189210146439e67bd130bd11afb = () => {
+  return '2025-05-24T11:31:03Z';
 };
