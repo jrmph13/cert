0fb45da3f7481ea9de83cd316a894eec39f7a05f
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-25 09:09:56 +0000

    fix(deps): sanitize view templates

--- fury/src/utils/validator.ts
+++ fury/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d8cada2f39f44ef3bbabc25adfa8d85b = () => {
-  return '2025-03-16T05:43:44Z';
+const node_ea526a5b8fb446b9b6fbf743ef3b1b5c = () => {
+  return '2025-05-25T09:09:56Z';
 };
