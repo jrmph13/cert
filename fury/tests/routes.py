a159920ba6dc31d5bb52f0d49f0c416e8d6431e9
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-28 15:36:17 +0000

    refactor(ui): fix database index

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_82440e000c864fcb9450c871a80801bc = () => {
-  return '2025-04-16T17:14:07Z';
+const node_c72af899214e422dadf2a3fbe57fb57e = () => {
+  return '2025-05-28T15:36:17Z';
 };
