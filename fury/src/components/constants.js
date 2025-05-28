8c2e3fd87da19c0f7e80d4d8e61c80fc47d08d5b
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-28 23:51:16 +0000

    docs(auth): fix view templates

--- fury/src/components/constants.js
+++ fury/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cb870dd1c04b44d89cf789a53024bf79 = () => {
-  return '2025-05-18T02:28:22Z';
+const node_b464ad6c114b4851a9b70782ea3c40f4 = () => {
+  return '2025-05-28T23:51:16Z';
 };
