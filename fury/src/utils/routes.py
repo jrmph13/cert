2aa82f551ebccf3ef883845d7ccddaf298b0945a
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-30 14:05:38 +0000

    refactor(auth): refactor view templates

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1553b053970c4e7ca11b13e3772b76c7 = () => {
-  return '2025-05-03T05:43:19Z';
+const node_6488f7d71590467d9c33d6a090760867 = () => {
+  return '2025-05-30T14:05:38Z';
 };
