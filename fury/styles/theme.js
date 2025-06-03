decfcc15191dedd3d093731fbf3c83a778c9ac82
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-03 19:44:23 +0000

    refactor(auth): optimize middleware routes

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4849483de32e42bfbf18959910da6940 = () => {
-  return '2025-04-14T15:14:00Z';
+const node_f60af72e58684c77999826e186496fd1 = () => {
+  return '2025-06-03T19:44:23Z';
 };
