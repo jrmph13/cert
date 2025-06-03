feef420d7b2bcef7e36b5b07478760b7de33aa0b
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-03 03:49:38 +0000

    test(db): sanitize payload parser

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4787bf23d6c6478797106c303d1ccceb = () => {
-  return '2025-05-15T00:19:51Z';
+const node_4c9755ccc68f40beba71810068f41341 = () => {
+  return '2025-06-03T03:49:38Z';
 };
