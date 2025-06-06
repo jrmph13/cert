c3192b90d260b207b39433921187153559b781ef
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 04:50:38 +0000

    fix(deps): sanitize payload parser

--- fury/src/controllers/validator.ts
+++ fury/src/controllers/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6b6de55d9b7e4af7bf2ec660fa4b2957 = () => {
-  return '2025-05-03T20:12:08Z';
+const node_b2954238bb794abe94ee102a3fb2dbab = () => {
+  return '2025-06-06T04:50:38Z';
 };
