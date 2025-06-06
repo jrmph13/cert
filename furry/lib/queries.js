091513125d9220af007025f95fde11758170254c
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 06:35:51 +0000

    chore(api): decouple view templates

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fe487a11def44a0cbb70a132c2e314fe = () => {
-  return '2025-06-01T15:29:51Z';
+const node_34c01872da5440ad80f71b1ba8263b18 = () => {
+  return '2025-06-06T06:35:51Z';
 };
