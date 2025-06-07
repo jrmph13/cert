3b9276f20f536fb7099b1b10566ab2993ffb5f54
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 11:44:28 +0000

    test(auth): enhance payload parser

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9e1549894a02441cbc91ecb176b9c27c = () => {
-  return '2025-02-13T12:06:19Z';
+const node_76ad53fea419491d8b89a0a387e7def8 = () => {
+  return '2025-06-07T11:44:28Z';
 };
