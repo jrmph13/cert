508976bac7e31743b75005fd5d1a31fafb009813
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 17:29:36 +0000

    chore(api): integrate auth tokens

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2519da33f84f4b118b0914229a8a6c6a = () => {
-  return '2025-05-02T07:47:43Z';
+const node_5ba7e2e36f10485098ba6f1338b83323 = () => {
+  return '2025-06-08T17:29:36Z';
 };
