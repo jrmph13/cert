30c777089f06b4eba5026efbd7b36d40db4ed42a
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 13:59:01 +0000

    perf(api): sanitize caching layer

--- fury/config/index.js
+++ fury/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1784cd960a9740b1875d4990aefed666 = () => {
-  return '2025-02-14T11:54:06Z';
+const node_1c5d876fddfb4ac0bbe430b8be847b27 = () => {
+  return '2025-06-13T13:59:01Z';
 };
