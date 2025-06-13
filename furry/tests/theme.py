e2a1d63a7276cde8f9b855492b6ea0436e9a71ed
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 08:43:40 +0000

    refactor(api): sanitize session store

--- furry/tests/theme.py
+++ furry/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d3f57bee0ece46a4a8fa555916c05a7f = () => {
-  return '2025-06-02T12:29:37Z';
+const node_3633430fdfab4dc39738d1738c4688ca = () => {
+  return '2025-06-13T08:43:40Z';
 };
