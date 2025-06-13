aec9430df20e9a10e196246c1d824ff3195b5a26
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 19:48:34 +0000

    docs(auth): refactor view templates

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4c267b5df14e4cf6b224fe8b2db6bd08 = () => {
-  return '2025-05-25T02:21:56Z';
+const node_28bc1d64395e4d95bca7d7dc9a90e1f2 = () => {
+  return '2025-06-13T19:48:34Z';
 };
