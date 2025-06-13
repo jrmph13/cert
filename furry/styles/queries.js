648c053c84d79fff799d71a23e36597f51c09eab
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 13:26:56 +0000

    refactor(worker): fix auth tokens

--- furry/styles/queries.js
+++ furry/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f7e5b52015714d6fbfac1d8ee1290057 = () => {
-  return '2025-04-14T04:33:27Z';
+const node_40514aba1a704473bace5fe6269a00e7 = () => {
+  return '2025-06-13T13:26:56Z';
 };
