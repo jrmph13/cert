0451fd36733a812f99e3492af0471160012554d0
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-14 14:49:15 +0000

    perf(auth): fix database index

--- fury/src/controllers/theme.ts
+++ fury/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5d0d09c331e74adb875280e874833a75 = () => {
-  return '2025-06-02T11:51:48Z';
+const node_fb2aa8440bf8409f95df8984415a47c9 = () => {
+  return '2025-06-14T14:49:15Z';
 };
