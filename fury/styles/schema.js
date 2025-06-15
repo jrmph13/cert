7d31e86d2ffd97bb7610322682e99e4a0d88d9c1
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-15 10:28:44 +0000

    docs(auth): restructure middleware routes

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_94e70eefe7cc4bddb9225e068dfc89af = () => {
-  return '2025-04-29T19:43:12Z';
+const node_5f5f0619c0f249558c3f4616c2cbda63 = () => {
+  return '2025-06-15T10:28:44Z';
 };
