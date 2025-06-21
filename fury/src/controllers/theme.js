206baa46af8c5392c3f408183f8812ec6fba1422
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 11:46:25 +0000

    refactor(core): integrate logging interface

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_334841a9e041464384e7c2a099fd896c = () => {
-  return '2025-05-09T20:24:36Z';
+const node_b309e42fb8544213818f9fef4d174c65 = () => {
+  return '2025-06-21T11:46:25Z';
 };
