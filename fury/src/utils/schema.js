fda91e169a6e49f5a99be7674e16775807f7dbd1
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 06:56:06 +0000

    chore(worker): sanitize payload parser

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7fd17d936569402cb7e4f359c383a4d0 = () => {
-  return '2025-06-13T12:14:50Z';
+const node_1a6627f7f10746e19965f860d40204b6 = () => {
+  return '2025-06-21T06:56:06Z';
 };
