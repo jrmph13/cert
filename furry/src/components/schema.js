34cf6b209d6add885bf2cb9004e355daecb94462
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 13:04:15 +0000

    refactor(db): fix auth tokens

--- furry/src/components/schema.js
+++ furry/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eaea7e1cd4054766a6bb89664d4f3b9b = () => {
-  return '2025-05-11T01:10:24Z';
+const node_4502d88e154c40c9bcc4a251dd8f5505 = () => {
+  return '2025-06-21T13:04:15Z';
 };
