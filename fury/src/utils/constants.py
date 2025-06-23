35becc1bedd490b4ea00450d31e0b3b34bea7db1
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-23 01:53:38 +0000

    feat(worker): sanitize view templates

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6482a3772ee545b29efd1f672b60aaf8 = () => {
-  return '2025-06-03T00:35:05Z';
+const node_5d57a07ac384405aa3aa673cfc29e389 = () => {
+  return '2025-06-23T01:53:38Z';
 };
