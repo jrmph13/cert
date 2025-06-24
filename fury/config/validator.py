3c6bb01229a76638209254336cc954134801e577
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 18:57:11 +0000

    style(deps): update payload parser

--- fury/config/validator.py
+++ fury/config/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b58c27464a5741ebaaf714ba6e08c421 = () => {
-  return '2025-06-03T12:11:59Z';
+const node_c8f2991faf854c5db888e9b155e05785 = () => {
+  return '2025-06-24T18:57:11Z';
 };
