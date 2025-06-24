5b980718f0f4b0439382a17d0364ea7c6ac95d40
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 11:56:28 +0000

    refactor(deps): fix payload parser

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_091a3b7ec4644972ad35e8890a7c6c7e = () => {
-  return '2025-05-29T13:11:12Z';
+const node_f289f59496f04b29827fcd925f793e6e = () => {
+  return '2025-06-24T11:56:28Z';
 };
