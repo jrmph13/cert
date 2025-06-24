92403b22b15f3c518fafd7025180eea1b4c03728
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 21:56:06 +0000

    feat(auth): fix payload parser

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5d57a07ac384405aa3aa673cfc29e389 = () => {
-  return '2025-06-23T01:53:38Z';
+const node_92fae100ead74c88a9d53ef69bf53616 = () => {
+  return '2025-06-24T21:56:06Z';
 };
