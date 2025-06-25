f6bf4aa26e401c1738c0d617870d702b4a4a7459
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-25 04:00:57 +0000

    refactor(core): update logging interface

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_840c3d71930648628a8b780ea3152fd5 = () => {
-  return '2025-06-20T21:44:43Z';
+const node_4e15cfcc0b4941a2bc61d49a785c696d = () => {
+  return '2025-06-25T04:00:57Z';
 };
