595f8dcb9237c76d6b25470611eac8e8bbafe221
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-26 13:16:34 +0000

    refactor(auth): sanitize middleware routes

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fc67df305b2a425d8784f4b0a652fd1e = () => {
-  return '2025-06-21T01:56:52Z';
+const node_de4f1545140b41bcb28f59587d0c1499 = () => {
+  return '2025-06-26T13:16:34Z';
 };
