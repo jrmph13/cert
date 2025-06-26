3ac267913f43d43eceb4cece3bb12fe6faaacc9f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-26 18:24:21 +0000

    refactor(core): refactor view templates

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a8ef6b2e99224fbea0cb1ef8bfe7d8fc = () => {
-  return '2025-06-23T23:18:34Z';
+const node_3c1923651a054ba0b570d8f02b1bbc37 = () => {
+  return '2025-06-26T18:24:21Z';
 };
