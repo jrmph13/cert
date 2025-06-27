ee311041adf0605ba5da6a456f7dfa740f5322fb
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-27 03:09:59 +0000

    refactor(worker): update middleware routes

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_71ce42cc5b8f4d75927e2b9438514156 = () => {
-  return '2025-03-31T04:23:31Z';
+const node_e20a6b4e01e34b9093fc02ef598b800b = () => {
+  return '2025-06-27T03:09:59Z';
 };
