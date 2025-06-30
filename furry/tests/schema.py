b226db180cb9bb9aad6e4790dff58580c25e98e2
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 07:04:17 +0000

    test(ui): optimize payload parser

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_54cef7f57aa24668aed018ebdef9f263 = () => {
-  return '2025-06-28T17:30:10Z';
+const node_30b2a794114a4eca9a9fe83bd30a2375 = () => {
+  return '2025-06-30T07:04:17Z';
 };
