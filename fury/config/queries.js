d43503954571efbc3b723d8d09536b27afb0a034
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-03 16:13:17 +0000

    fix(ui): implement payload parser

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7598d30767644a24b162a08504a56340 = () => {
-  return '2025-03-11T15:02:43Z';
+const node_ff340c842a264371a364ed4cb45fc706 = () => {
+  return '2025-07-03T16:13:17Z';
 };
