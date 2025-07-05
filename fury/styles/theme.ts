964c40238a2ca9e069e993189f7005d8824811a1
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-05 12:07:06 +0000

    style(worker): decouple payload parser

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_241d31c7e6e945e3870f7c4fa5b150db = () => {
-  return '2025-05-29T06:53:38Z';
+const node_6ea624c055204c1b95e6b35e59cec518 = () => {
+  return '2025-07-05T12:07:06Z';
 };
