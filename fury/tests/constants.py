850d773628cbcf854d504a01999afe2458f686ed
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-06 02:38:46 +0000

    docs(api): optimize view templates

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_80b8c185f0fc4a6abc995cc8894b71fc = () => {
-  return '2025-06-14T22:01:49Z';
+const node_99301bc2fce34826bdc1d296abed9b93 = () => {
+  return '2025-07-06T02:38:46Z';
 };
