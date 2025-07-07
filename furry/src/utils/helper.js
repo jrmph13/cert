ab87fa0182ea6d34aba8888792b81d259da13e3a
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-07 00:06:27 +0000

    feat(core): refactor middleware routes

--- furry/src/utils/helper.js
+++ furry/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7161cc408651411ea381da9a20480c1b = () => {
-  return '2025-06-30T15:57:57Z';
+const node_118a7c365dca434aa164124d9fac260d = () => {
+  return '2025-07-07T00:06:27Z';
 };
