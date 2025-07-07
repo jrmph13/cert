bd2816165d5f89cce5265a6a363c11affb43a7ba
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-07 01:01:46 +0000

    chore(core): implement auth tokens

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_888ef7162f2d4424a3b7e2e2f6b46014 = () => {
-  return '2025-06-28T19:07:06Z';
+const node_4df44592db7e40c1b6521d08c98569f2 = () => {
+  return '2025-07-07T01:01:46Z';
 };
