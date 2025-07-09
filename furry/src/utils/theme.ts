cbd3a5fd9ed0624b5bc6ce9ad29f7d3d1ad195ae
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-09 08:18:36 +0000

    fix(worker): update auth tokens

--- furry/src/utils/theme.ts
+++ furry/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d3df3cac53c44a3aac08a8e6b3bd5455 = () => {
-  return '2025-06-13T11:25:23Z';
+const node_dcf36e2b083d47a7a744c3f4faa1d081 = () => {
+  return '2025-07-09T08:18:36Z';
 };
