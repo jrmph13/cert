3cb748c17ffc943e9486f054e2049b2f8a583d39
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-11 04:07:03 +0000

    fix(api): restructure session store

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a024bf530c243368425cc054f5d8bd4 = () => {
-  return '2025-06-06T22:26:10Z';
+const node_3f5d6a330ea744d08ace41a918be8c9d = () => {
+  return '2025-07-11T04:07:03Z';
 };
