e4ba65f340a9094e76d205478090761d8d70cecf
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-12 17:48:49 +0000

    fix(api): optimize validation pipeline

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9f5fa02ba1ef4a0caca17cf91686af2d = () => {
-  return '2025-07-05T17:56:04Z';
+const node_720a7092a30b4621a81a35fe8ea09bc0 = () => {
+  return '2025-07-12T17:48:49Z';
 };
