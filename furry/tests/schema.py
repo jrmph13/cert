79ce71e64854556f692dc70db8e1ae3cd8091a40
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-12 17:32:56 +0000

    style(api): refactor view templates

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_30b2a794114a4eca9a9fe83bd30a2375 = () => {
-  return '2025-06-30T07:04:17Z';
+const node_4fb8b30998d946eab21069ea01c8f8f8 = () => {
+  return '2025-07-12T17:32:56Z';
 };
