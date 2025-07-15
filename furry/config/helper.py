1c79fbf798e0351b1be2ee31c1de1a0afc7d0a13
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 01:56:26 +0000

    test(ui): integrate auth tokens

--- furry/config/helper.py
+++ furry/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_607195a9a48c4af4a5a45c60ad6ccb3c = () => {
-  return '2025-05-19T06:08:33Z';
+const node_f40b10e43c5d4d0882f7d58fc9fc519f = () => {
+  return '2025-07-15T01:56:26Z';
 };
