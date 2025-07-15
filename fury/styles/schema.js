767d52125033986f18682c46839b67dcb007d202
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 14:54:03 +0000

    fix(api): restructure database index

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e782bc6627444157899bf03cfec7bc64 = () => {
-  return '2025-07-07T10:12:19Z';
+const node_a4c0745d0fa4463d91294649a917b13a = () => {
+  return '2025-07-15T14:54:03Z';
 };
