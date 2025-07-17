af318ecab25334e4b83ef20adb4fceea8c0f637d
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 05:47:14 +0000

    test(core): integrate logging interface

--- fury/config/constants.js
+++ fury/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_75e94642d10b4424a3d265363b0fc638 = () => {
-  return '2025-06-08T14:00:59Z';
+const node_6100183576a24da794d61d90739fb6a2 = () => {
+  return '2025-07-17T05:47:14Z';
 };
