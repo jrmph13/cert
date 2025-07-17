e34972e393fdcd859ca92769f505a2dbf94db828
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 10:00:21 +0000

    fix(core): decouple view templates

--- furry/src/controllers/queries.js
+++ furry/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ab19b6f9b7b747c283508b9d6437bb2b = () => {
-  return '2025-06-14T23:02:39Z';
+const node_938e288d53574d3b9070f08af29313ef = () => {
+  return '2025-07-17T10:00:21Z';
 };
