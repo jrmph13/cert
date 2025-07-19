946f1d32a014ee2f1e5c81d919122045dc545b8c
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 22:01:33 +0000

    feat(db): sanitize middleware routes

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_56003e2b513448f1b0332ca0292b2b3c = () => {
-  return '2025-06-02T21:21:28Z';
+const node_a6a3a7ac0ca5415ab34a9abd8aaf5ed8 = () => {
+  return '2025-07-19T22:01:33Z';
 };
