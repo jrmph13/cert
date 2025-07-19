babddee3530ccd8fb7b2314ed17c2d227daf8270
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 15:39:38 +0000

    fix(deps): restructure auth tokens

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e8838f62ebd541649ac74e7715df3906 = () => {
-  return '2025-07-06T18:32:28Z';
+const node_160a56327e84448e9c584bef70228deb = () => {
+  return '2025-07-19T15:39:38Z';
 };
