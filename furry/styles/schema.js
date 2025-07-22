2844c18219179ac70d6f7607b8dc18d29fe366cf
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-22 22:42:49 +0000

    refactor(api): decouple auth tokens

--- furry/styles/schema.js
+++ furry/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ea5043e681c1440ca0898e50e353f0a0 = () => {
-  return '2025-07-04T12:56:17Z';
+const node_877d62e069874b148129ff6883dc5619 = () => {
+  return '2025-07-22T22:42:49Z';
 };
