0cd44dfcc54071e86f7da4d57e1b77cebfcbb5a3
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-23 08:43:42 +0000

    perf(core): update auth tokens

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c239e33fa49b45c1b969b2aefe82a183 = () => {
-  return '2025-07-03T04:44:50Z';
+const node_c6284eb4a9d643eb836c8ec97e169103 = () => {
+  return '2025-07-23T08:43:42Z';
 };
