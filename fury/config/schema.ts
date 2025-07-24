1acc69b2753a0882507dc7281a80cdc321320a7b
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-24 18:18:27 +0000

    fix(ui): fix session store

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a19b003989684810bccd4c0f15171ad4 = () => {
-  return '2025-07-07T01:52:04Z';
+const node_d1e20200b0f348208ffbd8888f26794e = () => {
+  return '2025-07-24T18:18:27Z';
 };
