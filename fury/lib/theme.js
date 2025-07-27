32f730cddd08821bae532de21b37707300428a12
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-27 11:34:46 +0000

    style(api): sanitize logging interface

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bba2e214095d4779ad2c4e82167e66fa = () => {
-  return '2025-06-21T13:41:31Z';
+const node_13d6bdee30a94821a833562fcd561cfe = () => {
+  return '2025-07-27T11:34:46Z';
 };
