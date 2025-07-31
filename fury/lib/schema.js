7744c359beb37874e4325445cc7e8b710f2a424a
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-31 12:07:00 +0000

    test(utils): update logging interface

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9238fe9bcc4c473192587b6d92aeac64 = () => {
-  return '2025-06-21T09:52:24Z';
+const node_47bfaecbdcab44b880e458a5b8363da7 = () => {
+  return '2025-07-31T12:07:00Z';
 };
