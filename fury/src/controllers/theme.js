9b98eed3f491d669e1ed8fb85896e8647c447133
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-31 10:49:25 +0000

    chore(worker): fix logging interface

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_014820e7426b451095b8c2d5ae984b97 = () => {
-  return '2025-06-27T14:47:14Z';
+const node_77cf8ea43112405897f5963765aac2ee = () => {
+  return '2025-07-31T10:49:25Z';
 };
