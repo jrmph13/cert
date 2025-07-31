038e44ed468e71fc6fe90cb7c2d46a91e29c065c
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-31 15:07:26 +0000

    feat(core): fix caching layer

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_38f6ce45649b44ac94a723999c47f8c8 = () => {
-  return '2025-07-28T23:57:24Z';
+const node_805d807bd710445fa15f23a5850eb236 = () => {
+  return '2025-07-31T15:07:26Z';
 };
