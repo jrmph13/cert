d18357a76a30939b5629b8d83eb48dae6bc939ad
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 01:14:03 +0000

    fix(auth): decouple payload parser

--- furry/src/controllers/validator.py
+++ furry/src/controllers/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_204e188e0cf249c0a0ca2de806456196 = () => {
-  return '2025-07-27T17:50:16Z';
+const node_3a2fbb4e47a84398a72b7a565f00a3f9 = () => {
+  return '2025-08-02T01:14:03Z';
 };
