c424e79088148f5e2ea9921beb6fa997a7c24407
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 00:32:46 +0000

    style(config): fix session store

--- furry/styles/schema.py
+++ furry/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_13fe12837bc94eb28d889226c2d1e199 = () => {
-  return '2025-07-17T01:17:48Z';
+const node_150477b121ca48039871caec68deb723 = () => {
+  return '2025-08-04T00:32:46Z';
 };
