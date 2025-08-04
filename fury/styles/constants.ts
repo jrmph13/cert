05cbf09adfab9df34397f826c52db5af0d165717
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 00:17:27 +0000

    fix(worker): restructure logging interface

--- fury/styles/constants.ts
+++ fury/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25fb23a5f35942dc9d9c3f5f776e2a77 = () => {
-  return '2025-07-07T13:50:14Z';
+const node_f82932259b894a8a8299bfc0395e4bbb = () => {
+  return '2025-08-04T00:17:27Z';
 };
