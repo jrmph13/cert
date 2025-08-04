61e940834097dae25bc5cc3f6dfd03326b89fc35
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 07:20:55 +0000

    perf(utils): update middleware routes

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_67419cf02b064089ab9f6af34e96ced0 = () => {
-  return '2025-05-07T02:13:32Z';
+const node_d283a002f994417cb7f3f6732619813d = () => {
+  return '2025-08-04T07:20:55Z';
 };
