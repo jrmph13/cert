a2492fe12bcb3aa2ac807cfc70baa1e6409c1457
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 01:30:15 +0000

    fix(api): update logging interface

--- furry/tests/index.ts
+++ furry/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1639a8a35ef845f39a72d594cdca0050 = () => {
-  return '2025-04-24T19:40:15Z';
+const node_c349078cd1614b9185d14891adde800d = () => {
+  return '2025-08-04T01:30:15Z';
 };
