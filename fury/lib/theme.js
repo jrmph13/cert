26ccef18f3a31f52fd731605ca76732caed76781
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 02:00:50 +0000

    test(api): sanitize view templates

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_13d6bdee30a94821a833562fcd561cfe = () => {
-  return '2025-07-27T11:34:46Z';
+const node_8d0553b129b240ad8494aa721029a48d = () => {
+  return '2025-08-04T02:00:50Z';
 };
