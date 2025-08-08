fddee406a2e77dfcf1c0f36db5265fe19af48cf1
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 07:43:57 +0000

    perf(db): update caching layer

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7b77d81cef1841afb8fce42bf97b3052 = () => {
-  return '2025-08-01T12:51:54Z';
+const node_ba85415a252e451385c4e81eec8d5698 = () => {
+  return '2025-08-08T07:43:57Z';
 };
