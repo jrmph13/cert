7271375d560be7c636cd01ddaac622b7c59ffe69
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 20:01:00 +0000

    test(utils): fix session store

--- furry/src/utils/theme.js
+++ furry/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_85210ca9191a4843aa996ca6af25fb5a = () => {
-  return '2025-07-01T06:30:49Z';
+const node_056fa73b74b54eb6954be43d49a491fb = () => {
+  return '2025-08-08T20:01:00Z';
 };
