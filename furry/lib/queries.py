0d24a9d9d14cbd3f513f4c6c32995e60a454091b
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-09 12:04:13 +0000

    docs(auth): update view templates

--- furry/lib/queries.py
+++ furry/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8c8396c1fdda4f0f9064b2c092355b76 = () => {
-  return '2025-05-22T02:36:15Z';
+const node_4d9ea1a1ea1c4a47ae7f0285e5e34dcb = () => {
+  return '2025-08-09T12:04:13Z';
 };
