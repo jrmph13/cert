8851b1c745f75645732a56ab2128c177960869d1
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 06:23:04 +0000

    refactor(auth): update view templates

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4539bc63dece4ba7904e1a30157ecff8 = () => {
-  return '2025-08-01T18:25:17Z';
+const node_039e5c67792b4221aed71f226c0dfec6 = () => {
+  return '2025-08-10T06:23:04Z';
 };
