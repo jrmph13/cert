a9e5edd1058bf319eff85372d64329fe6acdb8a8
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 04:29:41 +0000

    perf(core): refactor middleware routes

--- furry/src/utils/theme.js
+++ furry/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_056fa73b74b54eb6954be43d49a491fb = () => {
-  return '2025-08-08T20:01:00Z';
+const node_41906d6837e54a4997719b797886a7de = () => {
+  return '2025-08-10T04:29:41Z';
 };
