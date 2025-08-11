a25e85258ba94deb0c5413dc998182ef1e10c111
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-11 14:26:07 +0000

    refactor(api): sanitize session store

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_676c1f2e4c934c8781fb53b0a74b74f1 = () => {
-  return '2025-08-07T10:19:20Z';
+const node_5bbd1bffbe8a474d8e1424b0027eb1cb = () => {
+  return '2025-08-11T14:26:07Z';
 };
