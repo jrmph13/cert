9e2045c0e7725129509e818a70a2723264054b5f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-11 08:02:29 +0000

    perf(config): fix auth tokens

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8d0553b129b240ad8494aa721029a48d = () => {
-  return '2025-08-04T02:00:50Z';
+const node_24e93191adb44853a32b05664228ef39 = () => {
+  return '2025-08-11T08:02:29Z';
 };
