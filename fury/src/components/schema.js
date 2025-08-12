270fb8c958999dcd04c7d381d41dccfc764a195e
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-12 02:34:40 +0000

    feat(auth): sanitize payload parser

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9f8eaace70e24f6dbcbf0dfaf1e60222 = () => {
-  return '2025-08-08T11:30:10Z';
+const node_e5ec97b587a849f084a4b488bf0659bf = () => {
+  return '2025-08-12T02:34:40Z';
 };
