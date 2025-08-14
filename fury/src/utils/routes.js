282137ef5b6fbeef98597dac7301991df82d3846
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-14 04:35:24 +0000

    fix(ui): optimize database index

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_720a7092a30b4621a81a35fe8ea09bc0 = () => {
-  return '2025-07-12T17:48:49Z';
+const node_61f4a5feee434a548dd343577d44b6ef = () => {
+  return '2025-08-14T04:35:24Z';
 };
