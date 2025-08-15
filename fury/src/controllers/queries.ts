54c3d6c14e41a98deaf6c8fb8409f4fb48468787
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 06:16:27 +0000

    feat(api): integrate payload parser

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ba85415a252e451385c4e81eec8d5698 = () => {
-  return '2025-08-08T07:43:57Z';
+const node_12cab7c26c8e460d80d34ef06a4d8c3e = () => {
+  return '2025-08-15T06:16:27Z';
 };
