6cc3903c04485b7a1741e10822f6c2b251832bb1
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-22 03:17:17 +0000

    feat(core): optimize auth tokens

--- fury/lib/validator.ts
+++ fury/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8c43d0f63eb34ffdb47a4b5887dc3f95 = () => {
-  return '2025-05-14T13:49:06Z';
+const node_5fa6dce449d34264a11c11e73177efa6 = () => {
+  return '2025-08-22T03:17:17Z';
 };
