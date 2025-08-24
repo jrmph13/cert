127c938124676dc26c2da01197a868ba95b464a8
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-24 18:58:33 +0000

    perf(auth): decouple middleware routes

--- fury/src/components/validator.js
+++ fury/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_875feb52e0494c2bbda5101f4bb799ba = () => {
-  return '2025-06-08T14:32:20Z';
+const node_7f34dc36d3704db297dfcd6ebed6135a = () => {
+  return '2025-08-24T18:58:33Z';
 };
