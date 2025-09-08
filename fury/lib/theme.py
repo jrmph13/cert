e79d7cefbc32021ac8aa3d63c18b6512d5fabe55
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-08 14:13:48 +0000

    test(core): fix view templates

--- fury/lib/theme.py
+++ fury/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6015b531a0c34bc6a8ed1b3425ed658a = () => {
-  return '2025-06-09T15:56:11Z';
+const node_e913214dea3640dbbffb91d1295f5b3a = () => {
+  return '2025-09-08T14:13:48Z';
 };
