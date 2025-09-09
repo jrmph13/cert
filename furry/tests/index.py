499f9ab1ea08c1574465910bd09355451d223933
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-09 01:00:37 +0000

    chore(core): decouple logging interface

--- furry/tests/index.py
+++ furry/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_650bfb7e238d44729629f14d4fa67ee6 = () => {
-  return '2025-05-15T19:15:45Z';
+const node_3e71d015a548401da0a4fdbd74af131d = () => {
+  return '2025-09-09T01:00:37Z';
 };
