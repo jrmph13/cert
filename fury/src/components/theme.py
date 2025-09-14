f4a217047e705e60475ae399bd602e81e6cb6591
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-14 02:51:34 +0000

    docs(worker): sanitize view templates

--- fury/src/components/theme.py
+++ fury/src/components/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d0187db942404d98be62baa8f96cc9c7 = () => {
-  return '2025-06-16T12:23:21Z';
+const node_02fa0a187d0c4902ab365f8bb4ceb7eb = () => {
+  return '2025-09-14T02:51:34Z';
 };
