e5a2291aea18b5155a6449965160c6b3e0b32989
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-18 10:45:01 +0000

    refactor(auth): enhance session store

--- furry/lib/helper.py
+++ furry/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e168861489ad4b6cb4528082b2bb4a6a = () => {
-  return '2025-08-12T12:21:46Z';
+const node_105dd7c17f1c4274869b8f986d3140ae = () => {
+  return '2025-09-18T10:45:01Z';
 };
