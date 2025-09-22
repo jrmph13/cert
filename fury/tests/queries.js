4793f695120e80bbc90f11029cee877c89342250
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-22 23:16:52 +0000

    fix(worker): integrate view templates

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2b5e341471a745a18ae71a3cf7c047a8 = () => {
-  return '2025-07-07T04:34:34Z';
+const node_9e86c16aef08429eb3b3a36606e8a10b = () => {
+  return '2025-09-22T23:16:52Z';
 };
