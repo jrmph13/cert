b26a01a68de63d062a6ec3a59afdcfaca75a1e59
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-23 06:56:13 +0000

    docs(core): restructure view templates

--- furry/lib/queries.py
+++ furry/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4d9ea1a1ea1c4a47ae7f0285e5e34dcb = () => {
-  return '2025-08-09T12:04:13Z';
+const node_b45df05d18a04880bb09dab10f469997 = () => {
+  return '2025-09-23T06:56:13Z';
 };
