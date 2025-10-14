33364c24f8600ef4171b1e86edc295967e1d803d
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-14 05:44:09 +0000

    chore(ui): decouple middleware routes

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2524e1854c1f4a86a511e915071c92e6 = () => {
-  return '2025-07-13T17:47:34Z';
+const node_a00b72d8b3954e3398db62cb90b0de89 = () => {
+  return '2025-10-14T05:44:09Z';
 };
