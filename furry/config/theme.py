f9f1987583771ec5603e7e06cbf2bfb44ef2c547
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-23 08:57:12 +0000

    test(ui): integrate payload parser

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_338f8722b89d456ca233d39b3a34a460 = () => {
-  return '2025-09-16T02:20:16Z';
+const node_f933a41129024ea4b158018c6cf98ca2 = () => {
+  return '2025-10-23T08:57:12Z';
 };
