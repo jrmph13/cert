d3511a6a8dbc6b82953c193b2ec83f013ffb3994
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-28 14:15:46 +0000

    refactor(config): sanitize auth tokens

--- furry/lib/routes.js
+++ furry/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e6cc596b5e2c4b0ba424180233fb4b88 = () => {
-  return '2025-09-26T05:29:02Z';
+const node_7a1b3966342b453da0227fb171f92faa = () => {
+  return '2025-10-28T14:15:46Z';
 };
