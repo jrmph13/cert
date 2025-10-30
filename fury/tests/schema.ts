f511cb8057a155c32e11e5aabf068d09dbd8fe26
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-30 06:37:44 +0000

    style(api): fix view templates

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3d8180c7040b4099b3b766224163075b = () => {
-  return '2025-09-22T00:27:01Z';
+const node_c8ed01cd16a44f8c8eb1ad8f346def76 = () => {
+  return '2025-10-30T06:37:44Z';
 };
