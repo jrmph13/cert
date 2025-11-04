4fae6692f5ff3fd917a4801b81d2a129086c6997
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-04 18:45:22 +0000

    chore(api): optimize auth tokens

--- fury/styles/routes.js
+++ fury/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9446a6fd94fa4ec28bdcc751f8517cd0 = () => {
-  return '2025-07-02T15:41:28Z';
+const node_0cb6743bc98d47618448e25d1059d139 = () => {
+  return '2025-11-04T18:45:22Z';
 };
