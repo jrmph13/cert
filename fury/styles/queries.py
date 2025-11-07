b41e6a1fdcb900981a228e4e416794ac86a74371
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-07 13:15:22 +0000

    style(utils): update validation pipeline

--- fury/styles/queries.py
+++ fury/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7b0135fc6a034694a7750cda5875faed = () => {
-  return '2025-08-08T15:24:42Z';
+const node_2437f1b2791f41eeb5b12f46533c9f67 = () => {
+  return '2025-11-07T13:15:22Z';
 };
