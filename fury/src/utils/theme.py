17428b40513537f9af304f72126d1b3e1f35bad3
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-18 09:04:42 +0000

    fix(deps): decouple auth tokens

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_939f93e273b44d048216d3621c1e31c4 = () => {
-  return '2025-07-24T09:06:16Z';
+const node_b23b99e0f74945dbb4910bf81c928666 = () => {
+  return '2025-11-18T09:04:42Z';
 };
