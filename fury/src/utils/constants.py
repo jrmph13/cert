fcfe1de98b01457febc582c6e2f35a2c66dbdae8
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-30 17:57:15 +0000

    test(config): integrate view templates

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bd6fdada02c44f6a9dcd7b27f8118088 = () => {
-  return '2025-07-01T20:24:10Z';
+const node_47e53126112349e3afff080567f0a37b = () => {
+  return '2025-11-30T17:57:15Z';
 };
