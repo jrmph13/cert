acab1b08f05b534124b453adf4872b93137b95e4
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-04 09:12:54 +0000

    fix(config): refactor auth tokens

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_23ab59146c404c30b0efa81e85f51c6f = () => {
-  return '2025-12-04T18:22:18Z';
+const node_3c0bdd0966684f7fad5ca51c2fdebd18 = () => {
+  return '2025-12-04T09:12:54Z';
 };
