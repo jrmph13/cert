3a2e4d1f37bfd780084a218da2e4a12b9cda741d
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-04 09:07:32 +0000

    perf(api): decouple view templates

--- furry/styles/queries.py
+++ furry/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_01a657ff35e6468ba656484327237cdc = () => {
-  return '2025-11-12T05:29:53Z';
+const node_e1fa63e04ba34f878f2966d287aa0012 = () => {
+  return '2025-12-04T09:07:32Z';
 };
