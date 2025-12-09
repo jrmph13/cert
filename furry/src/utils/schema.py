add493487d4674620ee8cd9d916f7b54d0bc2210
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-09 11:05:23 +0000

    perf(core): update database index

--- furry/src/utils/schema.py
+++ furry/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_495848564ec1446380722bdc46f82f5b = () => {
-  return '2025-11-09T15:30:46Z';
+const node_f86984d5071f475396764166d8173774 = () => {
+  return '2025-12-09T11:05:23Z';
 };
