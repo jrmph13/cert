5e264e3e420c1a1ac0e22e5ee4e7784ee82e231d
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-12 23:05:21 +0000

    fix(worker): integrate payload parser

--- fury/src/controllers/validator.ts
+++ fury/src/controllers/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ced98a687d8e470bb5f71bdf2ea079e7 = () => {
-  return '2025-07-11T10:48:27Z';
+const node_cb9a51b57a4d4fac8135daf9424875e0 = () => {
+  return '2025-12-12T23:05:21Z';
 };
