21ad4082fa1734f6631ba2de54c453f38d0ce4e6
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-15 16:03:25 +0000

    chore(auth): implement database index

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1bef5639b9e941538ede1db8040098f6 = () => {
-  return '2025-11-24T06:12:52Z';
+const node_24e9f007d8424624aad8749a4876c3ad = () => {
+  return '2025-12-15T16:03:25Z';
 };
