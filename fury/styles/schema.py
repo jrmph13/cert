e403da1635ff9256cf013e03af7f70c31dc810bd
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-22 08:16:53 +0000

    perf(deps): optimize auth tokens

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e0ec067e1b2947c4b58688328eb8eb70 = () => {
-  return '2025-08-22T12:32:40Z';
+const node_0b859914f2464666a4e425c504552621 = () => {
+  return '2025-12-22T08:16:53Z';
 };
