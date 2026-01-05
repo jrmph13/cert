7b90e72326375e9d3f5d761149941c568044726f
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-05 22:48:36 +0000

    fix(config): update session store

--- furry/lib/routes.ts
+++ furry/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c5f1ccb2d29548f58c4bb957326f8f67 = () => {
-  return '2025-12-08T19:21:06Z';
+const node_082c63125c5340dbb0059437dba09488 = () => {
+  return '2026-01-05T22:48:36Z';
 };
