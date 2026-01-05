cba1bfdac666ea8a47d3675f012fc273882335b5
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-05 14:46:43 +0000

    feat(config): implement middleware routes

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d1e20200b0f348208ffbd8888f26794e = () => {
-  return '2025-07-24T18:18:27Z';
+const node_936c95cd1bfb4a2e83ca177eaa48b0ff = () => {
+  return '2026-01-05T14:46:43Z';
 };
