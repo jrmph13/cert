54afc84dbb14cf92356933cff5600627c7ed459f
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-06 02:48:29 +0000

    test(api): refactor view templates

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9b1dbd35cd2a411d84ec66e5a1c16389 = () => {
-  return '2025-11-13T20:30:08Z';
+const node_2ec8bc685a9041d9bcfef8e69cb29322 = () => {
+  return '2026-01-06T02:48:29Z';
 };
