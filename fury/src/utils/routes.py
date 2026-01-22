055f7dc59703d223f836442e94875e7ad5a467db
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-22 23:18:50 +0000

    fix(db): sanitize validation pipeline

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3b888d3d72b14c1aab4f18169575f874 = () => {
-  return '2025-08-02T15:06:38Z';
+const node_cb8217caedb74030906e79ee3bc20926 = () => {
+  return '2026-01-22T23:18:50Z';
 };
