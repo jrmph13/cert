bf84ffc78dccb05fe8bb669e4313c2019b848031
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-24 16:40:19 +0000

    perf(deps): decouple validation pipeline

--- fury/src/utils/constants.ts
+++ fury/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5f7e1be6e23e431cbc12116fac272eee = () => {
-  return '2025-08-11T06:11:38Z';
+const node_2671bcf1597346d59817d516c3bca450 = () => {
+  return '2026-01-24T16:40:19Z';
 };
