541fa07b0b72dba418e586e5ae46882e03388c44
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-24 12:46:57 +0000

    fix(config): optimize payload parser

--- furry/tests/routes.py
+++ furry/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4240f5e31df742449a7fd650eed9b40f = () => {
-  return '2025-09-10T00:48:22Z';
+const node_dc278076623b46e68eabb343f0becf6e = () => {
+  return '2026-01-24T12:46:57Z';
 };
