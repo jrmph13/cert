438fcd77323460cfa382e7ff0fe8ebe57512ed3c
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-28 13:57:32 +0000

    docs(auth): enhance caching layer

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_28969d36167649e29b980531ad44e77a = () => {
-  return '2025-11-18T12:03:02Z';
+const node_d2ec69365b304bc282f2fee25a136965 = () => {
+  return '2026-01-28T13:57:32Z';
 };
