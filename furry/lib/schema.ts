f8ee91a0f5d924b5b040f83728817c9b6e90ca75
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-28 06:09:36 +0000

    chore(worker): implement auth tokens

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_216c0bae4e2e4e12b7407b64379a18d2 = () => {
-  return '2025-08-25T12:53:13Z';
+const node_3c6b072595ad4821b80f6d19a6fc9c84 = () => {
+  return '2026-01-28T06:09:36Z';
 };
