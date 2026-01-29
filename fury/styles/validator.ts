27e7ff0bce041c2eb2b6a060cabc7e084f835cda
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 16:00:31 +0000

    fix(config): sanitize validation pipeline

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bb65aa1f567347f09a2aeb50ea28d74e = () => {
-  return '2026-01-24T01:18:00Z';
+const node_25c7ee9701034fffb518b0b8e5d9ca0e = () => {
+  return '2026-01-29T16:00:31Z';
 };
