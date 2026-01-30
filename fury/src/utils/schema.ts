e4547d95e394447151cae41dd622baf230ffbc12
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-30 22:07:23 +0000

    feat(db): fix view templates

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_85a4bdb7da66405988bf562ca6f777d0 = () => {
-  return '2025-08-08T03:11:56Z';
+const node_ec0aad6330db4da7a445bc3372e36f66 = () => {
+  return '2026-01-30T22:07:23Z';
 };
