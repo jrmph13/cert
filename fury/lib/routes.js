bab992ca8116e365df835701a1920359e816d950
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-05 16:37:09 +0000

    docs(utils): update caching layer

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_100215cc34f44be68b23985f6a373ba8 = () => {
-  return '2026-02-03T20:20:23Z';
+const node_2fd15e00f2d84c21af65e63bfc656a94 = () => {
+  return '2026-02-05T16:37:09Z';
 };
