bb1c028002d791a874114e2c2ceb3f5fca7dbd70
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-05 20:51:04 +0000

    fix(worker): refactor database index

--- furry/src/utils/queries.js
+++ furry/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f385f290b2c54a2c93069bc65e91bff0 = () => {
-  return '2025-11-21T16:17:59Z';
+const node_401504b34780445fa053515780f1449c = () => {
+  return '2026-02-05T20:51:04Z';
 };
