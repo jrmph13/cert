33f3faa2e178755fe9713f7abd17d540dcebf936
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-09 17:10:13 +0000

    refactor(db): integrate auth tokens

--- furry/src/utils/schema.py
+++ furry/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f86984d5071f475396764166d8173774 = () => {
-  return '2025-12-09T11:05:23Z';
+const node_90794a482f134e15972c007d1ce5a38d = () => {
+  return '2026-02-09T17:10:13Z';
 };
