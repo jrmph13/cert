1c7a72ed00a283f4376cae6ea5c8569fc595446b
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-12 08:12:58 +0000

    fix(ui): update auth tokens

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c40ef386b22e4dcb9005f689089ef533 = () => {
-  return '2025-09-25T16:58:56Z';
+const node_f59bbc4df1c04ddab0eb8938eac232d5 = () => {
+  return '2026-02-12T08:12:58Z';
 };
