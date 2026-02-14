a90bc79bc126828c868633f49be042cbfe0873cf
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-14 11:41:12 +0000

    style(core): integrate auth tokens

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_116ae7aaa1a743a08416fd7e6684df5c = () => {
-  return '2025-08-10T23:31:19Z';
+const node_dc9d3ef3e782496a9851460ff43a6cec = () => {
+  return '2026-02-14T11:41:12Z';
 };
