b07c2d5435214fc7d26d24cbc87cb7a73a2e8f2a
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-14 17:47:05 +0000

    feat(config): restructure auth tokens

--- furry/styles/routes.ts
+++ furry/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dc01deb413d54e03892115950f191d44 = () => {
-  return '2025-11-18T20:54:44Z';
+const node_2519d34eee984c19a9133493cad607ac = () => {
+  return '2026-02-14T17:47:05Z';
 };
