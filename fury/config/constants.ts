ca760dc99831107e204a5579928cc83a200d9fc2
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-16 05:33:15 +0000

    style(core): decouple view templates

--- fury/config/constants.ts
+++ fury/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c1a14aa562c4c22acc97d17db0c161d = () => {
-  return '2026-02-04T18:45:18Z';
+const node_ce552c976eab4de6a7594a162ea3d8f3 = () => {
+  return '2026-02-16T05:33:15Z';
 };
