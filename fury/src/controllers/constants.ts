5c17d48afaf2c216bed08cdb6a4f259232ed965f
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-17 12:59:59 +0000

    fix(utils): sanitize caching layer

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b22d3128761b4ae08cc223b4d7f27b10 = () => {
-  return '2026-01-30T15:03:44Z';
+const node_2472949379e84dae9bafc8c88bf5420f = () => {
+  return '2026-02-17T12:59:59Z';
 };
