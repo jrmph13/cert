a0e720ece87f4008cbf570429631a2d04ed447c2
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-18 10:01:46 +0000

    feat(ui): update payload parser

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e5179cd937bb49be9f7d3ea1556390c8 = () => {
-  return '2026-02-18T01:50:33Z';
+const node_80e44392505f48309d53fe754e1d8497 = () => {
+  return '2026-02-18T10:01:46Z';
 };
