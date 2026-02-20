3aa2bfeeadf4c521c1db4dc311d4c88d29aa34c1
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-20 08:16:11 +0000

    docs(auth): sanitize caching layer

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_39bef348b4d042779701023d70c3beca = () => {
-  return '2026-02-13T02:41:18Z';
+const node_16e363d65240454b82f40815435d8fb3 = () => {
+  return '2026-02-20T08:16:11Z';
 };
