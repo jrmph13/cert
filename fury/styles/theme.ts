01f53d418f874ab22cc63f308c12cb3eef1f1b98
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-20 01:21:41 +0000

    fix(core): refactor session store

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_737e382745ba41e6962d68716d076f94 = () => {
-  return '2026-02-20T08:42:30Z';
+const node_03d2173ddf2f4cb2af0f3e23cbfc6f56 = () => {
+  return '2026-02-20T01:21:41Z';
 };
