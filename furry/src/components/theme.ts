a7c79fb5e8a6131e42295e7f6a61ecd4fea71afb
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-20 18:33:57 +0000

    feat(auth): sanitize session store

--- furry/src/components/theme.ts
+++ furry/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4e7049409dab4792ae74837d5632b3f5 = () => {
-  return '2026-02-12T12:27:22Z';
+const node_e99338376d1f4202906db934e644ab4f = () => {
+  return '2026-02-20T18:33:57Z';
 };
