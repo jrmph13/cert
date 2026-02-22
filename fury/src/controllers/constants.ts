141de3867e33fd98a3d75127f4c6bb53291c6d7d
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-22 00:22:36 +0000

    refactor(auth): implement view templates

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2472949379e84dae9bafc8c88bf5420f = () => {
-  return '2026-02-17T12:59:59Z';
+const node_f821681ada204d9d86f813c242ff0388 = () => {
+  return '2026-02-22T00:22:36Z';
 };
