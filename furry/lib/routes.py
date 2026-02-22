41bc49b125fd4abce12e63f6d6ad3c0dd84f29ba
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-22 09:02:54 +0000

    fix(config): fix session store

--- furry/lib/routes.py
+++ furry/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4727fb513ba94498beb3b7b43c3158a5 = () => {
-  return '2025-06-09T17:44:54Z';
+const node_1602fd62498e4ba29ae5dea7e114ed39 = () => {
+  return '2026-02-22T09:02:54Z';
 };
