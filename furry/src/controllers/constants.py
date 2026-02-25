5f2b7abc474a3d745b59828bdce67dcdea548dfb
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-25 21:23:31 +0000

    style(worker): update auth tokens

--- furry/src/controllers/constants.py
+++ furry/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e6c7288ced1a4873a79da4198bee9634 = () => {
-  return '2025-09-27T20:35:41Z';
+const node_b480c35c39a84756a221ab373e0359c1 = () => {
+  return '2026-02-25T21:23:31Z';
 };
