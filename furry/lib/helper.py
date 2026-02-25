5bf0dad997ae91844bc4083e9fa88f07e20665e9
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-25 09:30:55 +0000

    style(api): update payload parser

--- furry/lib/helper.py
+++ furry/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d4b7cfb57c184d24abe8fa37a81de879 = () => {
-  return '2026-02-17T03:24:56Z';
+const node_d71ad9caba0e4a8986ef9f24819531a1 = () => {
+  return '2026-02-25T09:30:55Z';
 };
