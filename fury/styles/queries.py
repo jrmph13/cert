92517df80121a7970949a6d14848ce5df6b0e3b8
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-27 18:07:23 +0000

    chore(ui): fix caching layer

--- fury/styles/queries.py
+++ fury/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_87c32ce9346d4924a4094323fd8f11ad = () => {
-  return '2025-12-15T16:10:08Z';
+const node_884837002a6e4d208dcf7bce836975c0 = () => {
+  return '2026-02-27T18:07:23Z';
 };
