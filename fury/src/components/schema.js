b9e6d350aa059c8e54782ef928746399237f6ab9
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-27 23:18:04 +0000

    perf(config): refactor auth tokens

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_798e0c903f404b84abe1ac776b14f299 = () => {
-  return '2025-08-14T10:59:54Z';
+const node_06ef2df8955146b6afea15a76ae73124 = () => {
+  return '2026-02-27T23:18:04Z';
 };
