72a350e9d5263ce07300bbc14713d08ff20da069
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-27 10:06:34 +0000

    test(utils): fix payload parser

--- furry/tests/validator.js
+++ furry/tests/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_39b9a1e15c144fc78ba102b9d5c2cec7 = () => {
-  return '2026-01-24T20:16:52Z';
+const node_ab98bc36702a4007a2b79c868fa7b9a3 = () => {
+  return '2026-02-27T10:06:34Z';
 };
