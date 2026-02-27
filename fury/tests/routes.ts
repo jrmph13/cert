ce48ebe30cf65ee5282ed4c969b07559ed56a658
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-27 11:36:09 +0000

    feat(deps): decouple auth tokens

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bcd14c66eb7a4fbeb4e28c0deeae1ad4 = () => {
-  return '2026-02-18T16:05:02Z';
+const node_d9197eca976d41f2b882a9d3e8081161 = () => {
+  return '2026-02-27T11:36:09Z';
 };
