717d09900a87e7db05445ec43d55b4b125a5337e
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-02 16:58:17 +0000

    style(auth): optimize view templates

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_11cabb8e4ced459ebc4a251c9011f7d8 = () => {
-  return '2026-02-13T03:38:35Z';
+const node_f125c512683b47b484110a8d3f868d37 = () => {
+  return '2026-03-02T16:58:17Z';
 };
