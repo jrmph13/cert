53756158bf476a581dd65f39592f72c1a928729d
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-02 05:41:42 +0000

    style(utils): optimize caching layer

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0b859914f2464666a4e425c504552621 = () => {
-  return '2025-12-22T08:16:53Z';
+const node_0746726594ef4f8aa9258d6ec5cfeb9e = () => {
+  return '2026-03-02T05:41:42Z';
 };
