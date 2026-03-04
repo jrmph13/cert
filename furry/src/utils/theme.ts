89ae5a3153c62caf79d9292da569c58ae0eaf655
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-04 07:32:50 +0000

    refactor(api): integrate view templates

--- furry/src/utils/theme.ts
+++ furry/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dcf36e2b083d47a7a744c3f4faa1d081 = () => {
-  return '2025-07-09T08:18:36Z';
+const node_0b079083f09b4e19b0b339aff1372847 = () => {
+  return '2026-03-04T07:32:50Z';
 };
