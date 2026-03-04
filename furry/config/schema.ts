47b8b3608de4db086c816fc710cdb8609e78cd20
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-04 19:58:16 +0000

    test(worker): restructure database index

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2df30d8a678144f7a2c222da7cc5cbc2 = () => {
-  return '2026-01-07T12:01:23Z';
+const node_5ae4b4a9d20e42d88c21e33e6164a2e9 = () => {
+  return '2026-03-04T19:58:16Z';
 };
