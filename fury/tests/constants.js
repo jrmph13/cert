79684881e8d764869b0660e77aa6f9a4515f7148
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-08 15:16:52 +0000

    chore(db): fix auth tokens

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e7f047df50a647cd8d7ed841f09b5a77 = () => {
-  return '2026-02-20T14:15:15Z';
+const node_1e2bc8fa7820431abeb62bb5f786feec = () => {
+  return '2026-03-08T15:16:52Z';
 };
