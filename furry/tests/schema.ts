ccbca54fcb0c881c44533a739fc71f08c189772e
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-15 02:54:44 +0000

    fix(utils): update auth tokens

--- furry/tests/schema.ts
+++ furry/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c697a4d97d62448dabe0330b9b03f8df = () => {
-  return '2026-01-29T06:41:24Z';
+const node_48a272ef94254fc4af6b916673f925d8 = () => {
+  return '2026-03-15T02:54:44Z';
 };
