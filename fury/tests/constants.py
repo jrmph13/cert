4fb19ef084e7e25e52b884fcbac924d0591887fe
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-18 18:48:31 +0000

    fix(utils): implement view templates

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_45444f9f471d4386b645bbb7280a0e2b = () => {
-  return '2026-03-04T11:34:25Z';
+const node_2a7cd6f8503a4fa283e1798fff7ae7c1 = () => {
+  return '2026-03-18T18:48:31Z';
 };
