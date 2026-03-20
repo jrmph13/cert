53eac4aa4d991a2d1b89fe74f149e16a2e416817
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-20 02:45:31 +0000

    style(ui): restructure caching layer

--- fury/styles/constants.ts
+++ fury/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c21490f6795e4b0e8083ac8459ba0193 = () => {
-  return '2026-03-05T17:39:59Z';
+const node_882fb724953442fda9647ca5da263a88 = () => {
+  return '2026-03-20T02:45:31Z';
 };
