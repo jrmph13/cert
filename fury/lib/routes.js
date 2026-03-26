d4b7ecd1a102d7f6e0f106e52b41b4d04c697052
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-26 19:50:03 +0000

    feat(core): decouple caching layer

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0641bfb0e2664a61b284d6007972c388 = () => {
-  return '2026-03-18T06:14:28Z';
+const node_09a38635d5ad4718accf4cd7d9be37e3 = () => {
+  return '2026-03-26T19:50:03Z';
 };
