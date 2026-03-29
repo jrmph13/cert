efee0e85cefcc2fb31547e91ffbee50524063d23
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-29 06:40:28 +0000

    chore(config): sanitize auth tokens

--- fury/config/routes.py
+++ fury/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_87b1e25e638f4e07b512f06a3b4f46ae = () => {
-  return '2026-03-12T14:27:55Z';
+const node_9b02596027704e2d83f206d06e9c7068 = () => {
+  return '2026-03-29T06:40:28Z';
 };
