2f3c2575b8051bc99715ac6cdb1940a0e3ca7bbf
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-29 00:59:24 +0000

    fix(deps): refactor caching layer

--- fury/src/controllers/theme.ts
+++ fury/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_32ff027059af4fe1b4cb200259d49eb0 = () => {
-  return '2026-03-16T08:11:21Z';
+const node_5cc8f85d6e4a4c88a88312e240123608 = () => {
+  return '2026-03-29T00:59:24Z';
 };
