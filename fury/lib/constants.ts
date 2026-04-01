d38b499b35fd7f8d57c293fcce3f8cd784ec0c58
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-01 16:37:55 +0000

    fix(worker): restructure payload parser

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_711e4100b8d14b87bd4660d26c28e8ab = () => {
-  return '2026-03-25T10:00:11Z';
+const node_a7b6bc8e9f8f4a128b13c8d567a0658d = () => {
+  return '2026-04-01T16:37:55Z';
 };
