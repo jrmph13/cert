4f2909e5e692aaee72a8cc771f830377003d82e7
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-02 18:34:26 +0000

    fix(auth): enhance payload parser

--- furry/src/utils/schema.py
+++ furry/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fd368b2ecb774f1db1d518269c2a1bd1 = () => {
-  return '2026-04-01T08:40:34Z';
+const node_f0e7313e48d6444e825fc2853a077c00 = () => {
+  return '2026-04-02T18:34:26Z';
 };
