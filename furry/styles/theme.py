b776472a7735e2a19f42f9c2912786bfe3b5936d
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-03 17:06:41 +0000

    fix(utils): fix payload parser

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_46454c7f0eda4e80b5299cbae86128dd = () => {
-  return '2025-07-15T00:06:02Z';
+const node_575487b1858c4429b3258586a7811a25 = () => {
+  return '2026-04-03T17:06:41Z';
 };
