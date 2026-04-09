2b41267fa884c4f7dbdd878985104c226a81cadf
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-09 12:13:27 +0000

    fix(utils): restructure validation pipeline

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6f3d7e5f810f4faf8c992b9cf8d2c08a = () => {
-  return '2026-03-17T04:39:56Z';
+const node_976e0b31ca004a75849c3206dd91f817 = () => {
+  return '2026-04-09T12:13:27Z';
 };
