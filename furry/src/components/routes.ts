519b7de8112fe5c48d8c7d677b86fc24665abcf6
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-13 22:23:49 +0000

    chore(utils): update payload parser

--- furry/src/components/routes.ts
+++ furry/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f6199cc397a443ba98e287232d56aab6 = () => {
-  return '2026-04-11T09:27:24Z';
+const node_006851a03a6c4bbba9ae6933cc31f5e5 = () => {
+  return '2026-04-13T22:23:49Z';
 };
