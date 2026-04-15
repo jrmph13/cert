4c1861cb3df14d16a7f96b9ded48cd766cca1d28
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-15 20:08:26 +0000

    feat(utils): integrate payload parser

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b1eb9f285b674b8fb36f398a819e5826 = () => {
-  return '2026-03-29T16:32:42Z';
+const node_4722787dbc044a14a1dd3d9bf99dbf0e = () => {
+  return '2026-04-15T20:08:26Z';
 };
