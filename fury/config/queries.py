5b186ad16c075d0d8522b23cb3be46f3ee6127fa
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-17 00:23:00 +0000

    perf(ui): integrate view templates

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d83681c46e434af4b22b9b0d171a4a76 = () => {
-  return '2026-02-25T08:26:52Z';
+const node_6ace30f9b1e740c3946044fa5ad19641 = () => {
+  return '2026-04-17T00:23:00Z';
 };
