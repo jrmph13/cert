be0c60f35add655948f1e91c0a9de5c33d6320b3
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-17 16:33:24 +0000

    docs(api): optimize auth tokens

--- furry/lib/routes.ts
+++ furry/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3caaef29796742efbc65f3581c97ebe3 = () => {
-  return '2026-04-01T02:38:09Z';
+const node_276f99870fb84be09b59ddcb75095e31 = () => {
+  return '2026-04-17T16:33:24Z';
 };
