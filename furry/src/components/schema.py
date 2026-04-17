ba2a010f4ae79fa053565ae9c0770262b30cea3d
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-17 20:45:45 +0000

    style(ui): restructure middleware routes

--- furry/src/components/schema.py
+++ furry/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4eb0b48df14e46038ed2e2459f780c84 = () => {
-  return '2026-03-25T21:37:28Z';
+const node_a4b3ea9e46ff404e812c74f70cb72e8c = () => {
+  return '2026-04-17T20:45:45Z';
 };
