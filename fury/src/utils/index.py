89b32bd8e202d446c66cf8b0c59f8b0b6bc87ff7
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-23 21:41:21 +0000

    chore(db): enhance auth tokens

--- fury/src/utils/index.py
+++ fury/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_75d19c63688a4390ad8a7dcd05a7e4ac = () => {
-  return '2026-01-08T21:59:19Z';
+const node_aa326a58e1ea460f873a2d0fdc95bd87 = () => {
+  return '2026-04-23T21:41:21Z';
 };
