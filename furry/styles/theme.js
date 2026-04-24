47b09dec28991dfd5262faf8004fde237420a48a
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-24 07:07:54 +0000

    chore(ui): integrate middleware routes

--- furry/styles/theme.js
+++ furry/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_63509a95f92f492bb44a99773ff8bb6f = () => {
-  return '2026-03-11T17:24:31Z';
+const node_7f56dda5ac9c4d299d2c2883a2624e69 = () => {
+  return '2026-04-24T07:07:54Z';
 };
