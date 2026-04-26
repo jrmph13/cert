17dba24ea10c6a1d5849a8c003ae500a76ae03c4
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-26 21:32:12 +0000

    fix(utils): enhance auth tokens

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c316abb2c62b49b7bcededf2c13adb81 = () => {
-  return '2026-03-05T22:33:07Z';
+const node_eec4f55b60414f9482974aee8ab74314 = () => {
+  return '2026-04-26T21:32:12Z';
 };
