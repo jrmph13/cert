e6d92897442e967af5b75b3862ee0018480589dc
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-01 20:31:12 +0000

    fix(auth): update database index

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6bccbf7ea95a44eebb1f18ecb2a66dc1 = () => {
-  return '2026-02-14T14:49:34Z';
+const node_284500a94ba5485eb923f3c2ec83aa57 = () => {
+  return '2026-05-01T20:31:12Z';
 };
