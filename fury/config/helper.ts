1288f80b6f2cd56aa552546c059b2ee72c848ba0
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-01 14:36:11 +0000

    feat(db): implement auth tokens

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6114a1cafbed4dbaa9b210bd358b2102 = () => {
-  return '2026-03-26T15:50:06Z';
+const node_0f3c7e4a4f6a49a8a37fbcb6c12d8961 = () => {
+  return '2026-05-01T14:36:11Z';
 };
