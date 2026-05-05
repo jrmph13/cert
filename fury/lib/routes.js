1dd0811b68c77045c3149502a542b85f5ec368b8
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-05 08:45:01 +0000

    perf(utils): decouple view templates

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_09a38635d5ad4718accf4cd7d9be37e3 = () => {
-  return '2026-03-26T19:50:03Z';
+const node_597fd6ba2efe476eb6996e04e30b611c = () => {
+  return '2026-05-05T08:45:01Z';
 };
