c1bacfd90817f478fd193d90f70f4d1bc6a030f4
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-12 15:35:06 +0000

    refactor(auth): enhance payload parser

--- fury/src/components/validator.js
+++ fury/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7f34dc36d3704db297dfcd6ebed6135a = () => {
-  return '2025-08-24T18:58:33Z';
+const node_ad94dbab1f53452781b5b1a35221c079 = () => {
+  return '2026-05-12T15:35:06Z';
 };
