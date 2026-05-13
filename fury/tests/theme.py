6685b9a9fe8aa10ec9355e1f34194ca1f8217886
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-13 09:12:11 +0000

    docs(ui): refactor validation pipeline

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa8fcebf0a15472b8bfa19bb415e5235 = () => {
-  return '2026-04-27T23:00:37Z';
+const node_d6b5605431a44a16935da3569d5b7d92 = () => {
+  return '2026-05-13T09:12:11Z';
 };
