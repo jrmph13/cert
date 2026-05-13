2ae60150c1c8fa4bb86294048763d6d1ac5a2629
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-13 14:54:20 +0000

    style(auth): decouple auth tokens

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e72460fccf064762b3deca3567361187 = () => {
-  return '2026-05-11T05:31:06Z';
+const node_0ada66f8a2f947c78a7f3a36a635d191 = () => {
+  return '2026-05-13T14:54:20Z';
 };
