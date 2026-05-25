4a5d48fd0ebf5ceb8c3be6a84f03b444c8509d1e
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-25 20:37:06 +0000

    style(api): enhance auth tokens

--- fury/tests/theme.ts
+++ fury/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b1eaa88c41144bc2a11ed6ab00ae7f81 = () => {
-  return '2026-04-11T21:01:06Z';
+const node_14c0499d678e4779b68a0bd53a9138e3 = () => {
+  return '2026-05-25T20:37:06Z';
 };
