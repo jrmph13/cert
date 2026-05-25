69ca910bc4cc8615268f672e729a15d904cec0d4
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-25 13:46:56 +0000

    perf(core): optimize logging interface

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a9f2e0de330b4dde847e380a76de7e00 = () => {
-  return '2026-05-22T23:14:55Z';
+const node_8379fefd5b83418789665d5524fc00f2 = () => {
+  return '2026-05-25T13:46:56Z';
 };
