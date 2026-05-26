e07e6830ecdc071959bc0042460ba384c0e38baa
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-26 03:38:15 +0000

    style(config): fix middleware routes

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e489020d166a421984faf0e03c45c23d = () => {
-  return '2026-05-14T10:32:21Z';
+const node_b66ed2d462314cbf902e9c11ee1563ca = () => {
+  return '2026-05-26T03:38:15Z';
 };
