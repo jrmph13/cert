afb936b41b5e3dc24983b897ba6df68f42fb28b1
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-27 22:10:02 +0000

    refactor(utils): integrate view templates

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9686a6d7231445ee8e1856ef53625c25 = () => {
-  return '2026-05-15T21:32:41Z';
+const node_65ffe00777fe447097bdc6009dde4dc7 = () => {
+  return '2026-05-27T22:10:02Z';
 };
