606a7ddb4e7ddadb25ff15f1129a68247c4a12dd
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-27 10:00:55 +0000

    feat(deps): refactor view templates

--- fury/styles/routes.ts
+++ fury/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a9ab390c8f2e46539c192d81f8ae183a = () => {
-  return '2026-04-09T11:25:05Z';
+const node_e172524d65964cee89ce04ba78e3aa49 = () => {
+  return '2026-05-27T10:00:55Z';
 };
