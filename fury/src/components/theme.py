634770d0f6b59992fa8cb94a09ada301ec39d152
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-01 02:59:31 +0000

    refactor(utils): decouple middleware routes

--- fury/src/components/theme.py
+++ fury/src/components/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7fa196f09c9a4209945810572597da46 = () => {
-  return '2026-03-14T03:29:44Z';
+const node_e789eab1dcb34150aee0933ce8b12e25 = () => {
+  return '2026-06-01T02:59:31Z';
 };
