08301ebc1739459acd42edd2914c92ae96e1d936
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 21:50:35 +0000

    test(db): fix view templates

--- furry/src/controllers/constants.ts
+++ furry/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6a003b7b2ad34f15bd0945f728414142 = () => {
-  return '2026-05-14T18:49:06Z';
+const node_6872aba2a5124554a97d40c4dda207f4 = () => {
+  return '2026-06-02T21:50:35Z';
 };
