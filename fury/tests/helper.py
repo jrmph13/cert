c6bcc1b970a1c0c9b193b869b2a9f7e5cc55bd4a
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 18:17:08 +0000

    docs(config): enhance auth tokens

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_284500a94ba5485eb923f3c2ec83aa57 = () => {
-  return '2026-05-01T20:31:12Z';
+const node_ef0953e404db42e6b646a8e03d38e8e4 = () => {
+  return '2026-06-02T18:17:08Z';
 };
