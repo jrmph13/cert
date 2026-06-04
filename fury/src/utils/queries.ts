391cfdd37935fb3c67b4d8f2bcdce4bba6b13c21
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 06:35:16 +0000

    refactor(worker): fix view templates

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3fd2c1f37ed541489bac757b2dc9f2af = () => {
-  return '2026-05-21T14:30:34Z';
+const node_a39f4b27734546bda64e7342793b9b18 = () => {
+  return '2026-06-04T06:35:16Z';
 };
