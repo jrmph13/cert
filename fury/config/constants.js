b4b344f9b4f1dacec23b4c4ebd6082258993efd9
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-05 14:03:43 +0000

    perf(worker): sanitize logging interface

--- fury/config/constants.js
+++ fury/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c0957f2f3034995a77d268d6e445ad5 = () => {
-  return '2026-05-19T19:51:34Z';
+const node_e56ed84a06a24f0fa20828f544cbade5 = () => {
+  return '2026-06-05T14:03:43Z';
 };
