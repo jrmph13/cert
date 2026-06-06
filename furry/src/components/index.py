b1e73be33d5867609015e6a7e12cfdd3ad5a4ffc
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-06 01:30:32 +0000

    refactor(core): sanitize auth tokens

--- furry/src/components/index.py
+++ furry/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fecd005b5d9f4a358937cd096bcbd189 = () => {
-  return '2026-04-01T01:33:01Z';
+const node_8450d48e1f974b1fb9368604d4f73e8e = () => {
+  return '2026-06-06T01:30:32Z';
 };
