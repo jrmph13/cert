ca6d80c7a99f1cf89a99f9032a82e300f2109fa2
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-06 11:09:03 +0000

    feat(api): refactor validation pipeline

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f201264338084851a946b4eef694db57 = () => {
-  return '2026-04-22T22:46:11Z';
+const node_87150db9519b4e15b55679f11e2b62ba = () => {
+  return '2026-06-06T11:09:03Z';
 };
