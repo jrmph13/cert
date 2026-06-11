46dc220cfa94f7275db683489b349aa82b90c157
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-11 02:57:28 +0000

    fix(api): integrate view templates

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dce050bd428843b685e677860cfde0dd = () => {
-  return '2026-05-05T12:00:45Z';
+const node_076d926a1b45449ba5b59f33ced72c9b = () => {
+  return '2026-06-11T02:57:28Z';
 };
