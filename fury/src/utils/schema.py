8a0d2137903ce125ea55ab88e9b14fbfd1151b7b
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 12:44:20 +0000

    refactor(utils): update session store

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_65ffe00777fe447097bdc6009dde4dc7 = () => {
-  return '2026-05-27T22:10:02Z';
+const node_869534c662954d328fd6ef0621a3e471 = () => {
+  return '2026-06-12T12:44:20Z';
 };
