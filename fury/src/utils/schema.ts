2dd3c020d8fd4673dd7cdb326b3c2df692515a73
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 11:14:41 +0000

    chore(deps): update database index

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_864a300fcdff43179a786591185fb45a = () => {
-  return '2026-06-09T14:03:01Z';
+const node_7184183c735847ab801817524bba9e10 = () => {
+  return '2026-06-12T11:14:41Z';
 };
