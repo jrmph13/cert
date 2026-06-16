b06aa7733e66e6e7e3331593ecffae58d6a6299a
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-16 07:16:34 +0000

    chore(deps): fix session store

--- furry/config/validator.ts
+++ furry/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f95264e598c34578bfdce59e4e63f82a = () => {
-  return '2026-05-27T13:57:02Z';
+const node_f7e54c08943f440c82bc3a84f0f2462c = () => {
+  return '2026-06-16T07:16:34Z';
 };
