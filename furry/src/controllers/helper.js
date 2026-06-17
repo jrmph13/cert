c0e50e2caf0f44c578a2faa7d9a0083bb518a41d
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-17 10:49:12 +0000

    chore(api): implement auth tokens

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2fd017da6b03479693a0035cf2d883a0 = () => {
-  return '2026-06-11T15:51:05Z';
+const node_fac85b9e08a04b7bb2613cdea19638b6 = () => {
+  return '2026-06-17T10:49:12Z';
 };
