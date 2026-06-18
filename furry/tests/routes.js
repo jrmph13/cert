936be8683fcfd09e5229143a652f179251a7930f
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-18 11:17:34 +0000

    perf(auth): restructure view templates

--- furry/tests/routes.js
+++ furry/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5007d64950ea4ba89cb699e9370fd154 = () => {
-  return '2026-05-13T21:28:08Z';
+const node_e0581b9f5e2a49e39759a9f054df7e20 = () => {
+  return '2026-06-18T11:17:34Z';
 };
