3e9674fea12d02e3fabdb8b6dfef9148447c19c3
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-18 20:10:54 +0000

    style(config): refactor session store

--- furry/config/theme.ts
+++ furry/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7deb77d360b94253869fa16bd3dc61bb = () => {
-  return '2026-05-15T07:04:05Z';
+const node_d3c937fec0ad43a29b26e543bff0dc83 = () => {
+  return '2026-06-18T20:10:54Z';
 };
