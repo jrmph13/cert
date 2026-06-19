32095f73f45bf2d46dd6400473fdd2873daf5959
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-19 15:10:51 +0000

    docs(worker): implement caching layer

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_06dea8fd2d124bfa97ba3419a754c30e = () => {
-  return '2026-04-22T02:02:37Z';
+const node_d110527f08d34882967dacdf736cb621 = () => {
+  return '2026-06-19T15:10:51Z';
 };
