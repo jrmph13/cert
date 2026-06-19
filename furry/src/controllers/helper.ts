517d3e728c895767df3e3e07ffff7a3405663d6e
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-19 09:37:49 +0000

    fix(worker): update payload parser

--- furry/src/controllers/helper.ts
+++ furry/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a3a297c480e9478f8bf98389d6571435 = () => {
-  return '2026-06-11T19:07:55Z';
+const node_601c7647fccc4a1896c7452ea2c9ff2a = () => {
+  return '2026-06-19T09:37:49Z';
 };
