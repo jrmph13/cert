b477ed64a6483f7abd0a60721bc662e7059da240
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-20 23:00:54 +0000

    fix(core): fix database index

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_31cfabd3a4b94c53984b6f47e9aeff4f = () => {
-  return '2026-04-17T02:36:08Z';
+const node_5beb7a6b3c064fd5bb57766d2b0e40b1 = () => {
+  return '2026-06-20T23:00:54Z';
 };
