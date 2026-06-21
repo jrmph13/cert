80cf98de305fc914007d1581d6d4e9c8eacf8f2d
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-21 17:02:20 +0000

    refactor(worker): sanitize session store

--- furry/styles/constants.py
+++ furry/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c16275fa74045978f79e90e5df9cad6 = () => {
-  return '2025-09-10T12:55:31Z';
+const node_9fff8fdc78c549ea8a5336c2c407ffa1 = () => {
+  return '2026-06-21T17:02:20Z';
 };
