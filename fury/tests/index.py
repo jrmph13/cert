bded5e56849030d81e0eff0ec4a5941081fc0c13
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-21 14:58:29 +0000

    docs(core): decouple payload parser

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f12f336decda47918c34e9d4c279ea6d = () => {
-  return '2026-04-21T13:54:58Z';
+const node_5430202e18454b97a1918ff901f2532f = () => {
+  return '2026-06-21T14:58:29Z';
 };
