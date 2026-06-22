d33ead94a649f28076400572a06e690ccd894c90
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-22 19:40:25 +0000

    test(worker): update database index

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0eb3885b8c2541adaffb77ef5e4e8eb6 = () => {
-  return '2026-06-16T05:20:24Z';
+const node_372dda235d6c4a67a8d9f4c869f165f6 = () => {
+  return '2026-06-22T19:40:25Z';
 };
