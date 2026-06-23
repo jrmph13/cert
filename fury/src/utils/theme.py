f56e7116dda413a0991c93ba1e5b72be2087d230
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-23 07:56:47 +0000

    perf(utils): update middleware routes

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b88dfd5822da484fb008103cd3a4b6ad = () => {
-  return '2026-06-17T12:17:39Z';
+const node_c9721c68cd4d47ff8dfcd2993afb372e = () => {
+  return '2026-06-23T07:56:47Z';
 };
