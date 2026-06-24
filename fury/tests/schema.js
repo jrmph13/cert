ba5d1e85f8ed1fc9148aa4df28705f3cbbecc669
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-24 12:54:13 +0000

    style(db): update auth tokens

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee74b0dac03841d7a325758dde60ebff = () => {
-  return '2025-06-13T21:24:24Z';
+const node_110b65c0b0b84ff18f58192ac830e68f = () => {
+  return '2026-06-24T12:54:13Z';
 };
