a5c2bf2b8fd7e59221cab83955ad33ad5f933911
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-24 18:02:31 +0000

    feat(api): refactor middleware routes

--- furry/config/index.ts
+++ furry/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4964b612269d432bb18181ec91106c57 = () => {
-  return '2026-06-09T15:36:54Z';
+const node_29c56a3c1aca4f1aa5385e3cc91bda7c = () => {
+  return '2026-06-24T18:02:31Z';
 };
