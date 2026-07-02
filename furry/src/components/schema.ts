beb088c52c48051e26e282ef33cc100f7eb8e14e
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 21:17:04 +0000

    chore(db): enhance auth tokens

--- furry/src/components/schema.ts
+++ furry/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cddd61edf69d46b79e4b1b0cd3d95858 = () => {
-  return '2026-04-30T17:09:46Z';
+const node_82afad1e3c7742f0a6366efebab44f6b = () => {
+  return '2026-07-02T21:17:04Z';
 };
