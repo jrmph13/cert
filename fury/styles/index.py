8551a429b773029be66c3bf74c4bbf646b90be88
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 08:10:35 +0000

    chore(deps): decouple payload parser

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cb3d99e5acaa47969564d5de63953237 = () => {
-  return '2026-06-26T20:48:19Z';
+const node_b0a2ef183a8e41ef8fba3fe503d07c73 = () => {
+  return '2026-07-02T08:10:35Z';
 };
