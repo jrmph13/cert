949f58cd9a02a12a7cf644f707998d86f6930221
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 17:38:17 +0000

    style(utils): sanitize view templates

--- furry/lib/validator.ts
+++ furry/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4baf8fcdef234336af6cea54d4cab037 = () => {
-  return '2026-05-06T07:41:19Z';
+const node_06a4024d605643b9914f2adddc85bba9 = () => {
+  return '2026-07-02T17:38:17Z';
 };
